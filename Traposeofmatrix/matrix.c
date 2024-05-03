2d5f6c407c058664cd6c2273f0467503218a4e9e
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-05-03 09:23:47 +0530

    Add new files

---
+++ Traposeofmatrix/matrix.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+    int row,col,i,j;
+    printf("Enter the number of row for matrix : ");
+    scanf("%d",&row);
+    printf("Enter the number of col for matrix : ");
+    scanf("%d",&col);
+
+    int mat[row][col],trans[col][row];
+
+    printf("Enter the element of matrix\n");
+    for(i=0; i<row; i++){
+        for(j=0; j<col; j++){
+            scanf("%d",&mat[i][j]);
+        }
+    }
+
+    printf("Element of matrix\n");
+    for(i=0; i<row; i++){
+        for(j=0; j<col; j++){
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0; i<row; i++){
+        for(j=0; j<col; j++){
+            trans[j][i]=mat[i][j];
+        }
+    }
+
+    printf("Transpose Element\n");
+    for(i=0; i<row; i++){
+        for(j=0; j<col; j++){
+            printf("%d ",trans[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
