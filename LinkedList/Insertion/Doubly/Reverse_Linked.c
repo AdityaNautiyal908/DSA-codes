fe25c9e55aa23bf6dcde8fed63fa8992b323de33
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-05-04 20:55:08 +0530

    Add

---
+++ LinkedList/Insertion/Doubly/Reverse_Linked.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+void printLinkedList(struct node *ptr){
+    while(ptr!=NULL){
+        printf("%d ",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("\n");
+}
+    struct node *head, *first, *second, *third,*temp=NULL;
+
+void ReverseLists(struct node *temp2){
+    while(head!=NULL){
+        temp2=head->next;
+        head->next=temp;
+        temp=head;
+        head=temp2;
+    }
+    head=temp;
+}
+
+int main(){
+    first=malloc(sizeof(struct node));
+    second=malloc(sizeof(struct node));
+    third=malloc(sizeof(struct node));
+
+    head=first;
+
+    first->data = 1;
+    first->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next=NULL;
+
+
+    printf("Original Linked List\n");
+    printLinkedList(head);
+
+    ReverseLists(head);
+    printf("Reverse Linked List\n");
+
+    printLinkedList(head);
+
+}
