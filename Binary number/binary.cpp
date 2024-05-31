18dc90ceee9b5a52c87cdc8a89ce4f6c76b5c851
AdityaNautiyal908 <nautiyaladitya7@gmail.com>
2024-05-31 09:37:17 +0530

    Add

---
+++ Binary number/binary.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a = 4, b = 6;
+    // cout<<"AND operator : "<<(a & b)<<endl;
+    // cout<<"OR operator : "<<(a | b)<<endl;
+    // cout<<"NOT operator : "<<(~a)<<endl;
+    // cout<<"XOR operator : "<<(a^b)<<endl;
+
+    // cout<<"Left shift : "<<(13<<1)<<endl;
+    // cout<<"Left shift : "<<(13<<2)<<endl;
+    // cout<<"Right shift : "<<(13>>1)<<endl;
+    // cout<<"Right shift : "<<(13>>2)<<endl;
+
+    // Practice question
+
+    // Q1
+    // int a , b = 1;
+    // a = 10;
+    // if(++a)
+    // cout<< b;
+    // else 
+    // cout<< ++b;
+
+    // int a = 1;
+    // int b = 2; 
+
+
+    // Q2
+    // if(a-- > 0 && ++b > 2){
+    //     cout<<"Stage1 - Inside if"<<endl;
+    // }
+    // else {
+    //     cout<<"Stage2 - Inside else"<<endl;
+    // }
+    // cout<< a <<" "<< b << endl; // 0 and 3
+
+
+    //Q3
+    // if(a-- > 0 || ++b > 2){
+    //     cout<<"Stage1 - Inside if"<<endl;
+    // }
+    // else {
+    //     cout<<"Stage2 - Inside else"<<endl;
+    // }
+    // cout<< a <<" "<< b << endl; // 0 and 2 because of short-circuit in logical OR gate expression
+
+
+    //Q4
+    // int number = 3;
+    // cout << (25 * (++number) ); // 100 
+
+
+    //Q5
+    // int a = 1;
+    // int b = a++;
+    // int c = ++a;
+    // cout<< b <<endl; // 1
+    // cout<< c <<endl; // 3
+
+    return 0;
+}
